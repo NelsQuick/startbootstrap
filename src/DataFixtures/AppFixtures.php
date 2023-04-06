dae7ad4a9f46f1943cd1f7b833c0d4caff0d3d11
NelsQuick <nelsonpires.pro@gmail.com>
2023-04-06 13:11:37 +0200

    nickel +10

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,14 +4,21 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use App\Entity\Project;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        for ($i = 0; $i < 10; $i++) {
+            // Ajoute des livres à la base de données
+            $project = new Project();
+            $project->setTitle('Project ' . $i)
+                ->setDescription('lorem ipsum')
+                ->setImage('Lorem Ipsum');
+            $manager->persist($project);
 
         $manager->flush();
     }
+}
 }
